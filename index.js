1b738ef5cf17fd89e5e285a05dbdfbb74db4c6a0
Belen <belenjarabanavas31@gmail.com>
2022-06-08 12:08:40 +0200

    Classes: occupancyPercentage finished

--- index.js
+++ index.js
@@ -7,11 +7,23 @@ class Room {
   }
 
   isOccupied(date) {
-    this.bookings.forEach(booking => {
-        date >= booking.check_in && date <= booking.check_out
-        ? (booking.name)
-        : false
+    this.bookings.forEach((booking) => {
+      // if the date matches a date's reservation
+      date >= booking.check_in && date <= booking.check_out
+        ? booking.name // return the guest's name
+        : false; // otherwise, return false
+    });
+  }
 
+  occupancyPercentage(startDate, endDate) {
+    let reservedBookings = []; // array for the reserved bookings
+    let totalNumberOfBookings = this.bookings.length; // total number of the bookings
+
+    this.bookings.forEach((booking) => {
+      startDate >= booking.check_in && endDate <= booking.check_out; // if the range of dates provided matches with the dates
+      reservedBookings.push(booking); // add the reservations at bookingsReserved
+
+      return Math.round((reservedBookings.length / totalNumberOfBookings) * 100); // and return the rounded percentage of the occupancy
     });
   }
 }
@@ -25,4 +37,6 @@ class Booking {
     this.discount = discount; // int percentage
     this.room = room; // a room object
   }
+
+  
 }
