7c25759d70175d718da9e602ce826e8b77a11917
Belen <belenjarabanavas31@gmail.com>
2022-06-09 09:28:10 +0200

    Tests: isOccupied updated and done

--- index.test.js
+++ index.test.js
@@ -4,33 +4,36 @@ const templateRoomExample = {
   name: "suite",
   bookings: [],
   rate: 230,
-  discount: 5,
+  discount: 0,
 };
 
 const bookingTemplateExample = {
   name: "Belén Jaraba",
   email: "test@jest.com",
-  check_in: new Date("20 May 2022 14:00 UTC"),
-  check_out: new Date("25 May 2022 14:00 UTC"),
-  discount: 15,
-  room: "suite",
+  check_in: "20 May 2022 14:00 UTC",
+  check_out: "25 May 2022 14:00 UTC",
+  discount: 0,
+  room: { ...templateRoomExample },
 };
 
-/* isOccuped() */
-
 describe("Room and Booking", () => {
-  test("debe devolver el nombre de la habitación", () => {
-    const firstRoomExample = new Room({ ...templateRoomExample });
-    expect(firstRoomExample.name).toBe("suite");
+  /* isOccuped() */
+  test("If the room is not occupied and the bookings are empty", () => {
+    const room = new Room({ ...templateRoomExample });
+    expect(room.isOccupied("5 Oct 2021 14:00 UTC")).toBeFalsy();
   });
 
-  test("debe devolver false si la habitación no está ocupada en esa fecha. Sino el nombre del huesped", () => {
+  test("If the room is not ocuppied, return false", () => {
     const booking = new Booking({ ...bookingTemplateExample });
     const room = new Room({ ...templateRoomExample, bookings: booking });
     room.bookings = [booking];
-    expect(room.isOccupied(new Date("20 May 2022 14:00 UTC"), new Date("25 May 2022 14:00 UTC"))).toBe("Belén Jaraba");
+    expect(room.isOccupied("1 Jan 2019 14:00 UTC")).toBeFalsy();
   });
 
-
+  test("If the room is occupied, return the name of th guest", () => {
+    const booking = new Booking({ ...bookingTemplateExample });
+    const room = new Room({ ...templateRoomExample, bookings: booking });
+    room.bookings = [booking];
+    expect(room.isOccupied("25 May 2022 14:00 UTC")).toBe("Belén Jaraba");
+  });
 });
-
